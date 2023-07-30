aff68fa88417902be22239a4d29fe7750e403f10
prakash8876 <prakash8876@gmail.com>
2023-07-30 12:05:05 +0530

    #1: all service added and working

--- gson-demo/gson-demo/src/main/java/io/matoshri/gson/controller/UsersController.java
+++ gson-demo/gson-demo/src/main/java/io/matoshri/gson/controller/UsersController.java
@@ -4,10 +4,9 @@ import io.matoshri.gson.dto.UserDTO;
 import io.matoshri.gson.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -19,5 +18,35 @@ public class UsersController {
     @PostMapping("from-json")
     public ResponseEntity<List<UserDTO>> saveUsersFromJson() {
         return ResponseEntity.ok(userService.saveFromJson());
+    }
+
+    @GetMapping("email")
+    public UserDTO getUserByEmail(@RequestParam(name = "email", required = true) String email) {
+        return userService.getUserByEmail(email);
+    }
+
+    @GetMapping("username/{username}")
+    public UserDTO getUserByUsername(@PathVariable(value = "username") String username) {
+        return userService.getUserByUsername(username);
+    }
+
+    @GetMapping("by-age/{age}")
+    public List<UserDTO> getAllUsersByAge(@PathVariable(value = "age") int age) {
+        return userService.getAllUsersByAge(age);
+    }
+
+    @GetMapping
+    public List<UserDTO> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @PostMapping("save")
+    public UserDTO saveUser(@Valid @RequestBody UserDTO userDTO) {
+        return userService.saveUser(userDTO);
+    }
+
+    @PostMapping("save-all")
+    public List<UserDTO> saveUsers(@RequestBody @Valid List<UserDTO> userDTOS) {
+        return userService.saveUsers(userDTOS);
     }
 }
