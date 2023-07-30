fa040ac9a149872bd129bd069acd05a30d74629d
prakash8876 <prakash8876@gmail.com>
2023-07-30 09:48:53 +0530

    gson: project added

---
+++ gson-demo/gson-demo/src/test/java/io/matoshri/gson/GsonDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.matoshri.gson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsonDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
